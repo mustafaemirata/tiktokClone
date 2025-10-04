af8456b49955101441d9a961a23500d4e9e38e67
Mustafa Emir Ata <mustafaemirata@outlook.com>
2025-10-04 11:23:45 +0300

    upload

--- lib/authentication/authentication_controller.dart
+++ lib/authentication/authentication_controller.dart
@@ -4,7 +4,6 @@ import 'package:cloneshorts/global.dart';
 import 'package:cloneshorts/home/home_screen.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/foundation.dart';
 import 'package:get/get.dart';
